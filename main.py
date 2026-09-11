8aa66137294d7406bf7258c8b900808ea9aedf6d
jonquil_gt <anand.r.nallathambi@gmail.com>
2026-09-11 15:29:00 -0400

    A: All of my initial Asteroids project

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    #Initialize pygame
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    #Setup clock to enable FPS limit
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    #Build screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    color = (0,0,0)
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color)
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
+        # print(dt)
+
+
+if __name__ == "__main__":
+    main()
